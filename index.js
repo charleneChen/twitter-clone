f7664aa89b92df8ebce714ce231a93875e33c6eb
Charlene.Chen <satikey@gmail.com>
2025-11-20 12:04:23 +1100

    change for of to forEach()

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ import { tweetsData } from "./data.js";
 
 const tweetInput = document.getElementById("tweet-input");
 const tweetBtn = document.getElementById("tweet-btn");
-const feed = document.getElementById("feed");
 
 tweetBtn.addEventListener("click", function () {
     console.log(tweetInput.value);
 });
 
 function getFeedHtml() {
-    let feedHtml = "";
+    let feedHtml = ``;
+
+    /*
     for (let tweet of tweetsData) {
+        feedHtml += `
+            <div class="tweet">
+                <img src="${tweet.profilePic}" alt="A profile picture of user ${tweet.handle}" class="profile-pic">
+                <div>
+                    <p class="tweet-handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">${tweet.replies.length}</span>
+                        <span class="tweet-detail">${tweet.likes}</span>
+                        <span class="tweet-detail">${tweet.retweets}</span>
+                    </div> 
+                </div>
+            </div>
+        `;
+    }
+    */
+
+    tweetsData.forEach(function (tweet) {
         feedHtml += `
             <div class="tweet">
                 <img src="${tweet.profilePic}" alt="A profile picture of user ${tweet.handle}" class="profile-pic">
@@ -25,8 +44,12 @@ function getFeedHtml() {
                 </div>
             </div>
         `;
-    }
+    });
     return feedHtml;
 }
 
-feed.innerHTML = getFeedHtml();
+function renderFeedHtml() {
+    document.getElementById("feed").innerHTML = getFeedHtml();
+}
+
+renderFeedHtml();
