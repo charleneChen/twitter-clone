8b370ec14a172fc33549a3e565e67d7219736a86
Charlene.Chen <satikey@gmail.com>
2025-11-20 15:54:06 +1100

    Move tweetInput variable locally

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from "./data.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
-const tweetInput = document.getElementById("tweet-input");
-
 document.addEventListener("click", function (e) {
     if (e.target.dataset.like) {
         handleLikeClick(e.target.dataset.like);
@@ -51,6 +49,7 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
+    const tweetInput = document.getElementById("tweet-input");
     if (tweetInput.value) {
         tweetsData.unshift({
             handle: `@Charlene 😍`,
