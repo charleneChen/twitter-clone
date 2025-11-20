6635a94ec7a1268faac4ae1889dca1d200341a49
Charlene.Chen <satikey@gmail.com>
2025-11-20 11:39:19 +1100

    iterate over the data and create HTML string for each tweet

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ import { tweetsData } from "./data.js";
 
 const tweetInput = document.getElementById("tweet-input");
 const tweetBtn = document.getElementById("tweet-btn");
+const feed = document.getElementById("feed");
 
 tweetBtn.addEventListener("click", function () {
     console.log(tweetInput.value);
 });
+
+function getFeedHtml() {
+    let feedHtml = "";
+    for (let tweet of tweetsData) {
+        feedHtml += `
+            <div class="tweet">
+                <img src="${tweet.profilePic}" alt="A profile picture of user ${tweet.handle}" class="profile-pic">
+                <div>
+                    <p class="tweet-handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">${tweet.replies.length}</span>
+                        <span class="tweet-detail">${tweet.likes}</span>
+                        <span class="tweet-detail">${tweet.retweets}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    return feedHtml;
+}
+
+feed.innerHTML = getFeedHtml();
